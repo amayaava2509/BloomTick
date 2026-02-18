7e8340a93b533e0dd6ba136220c6b4e9e429375e
amayaava2509 <nashnaja2509@gmail.com>
2026-02-19 00:32:54 +0530

    Add service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = "bloomtick-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "icon_96.png"
+  "icon_128.png"
+  "icon_192.png"
+  "icon_384.png"
+  "icon_512.png"
+  "icon_1024.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+
